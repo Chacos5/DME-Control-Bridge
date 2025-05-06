d3e671888be5b915e2ff53e961bdf1bb535d44e5
Grayden <grayden.pope@gmail.com>
2025-05-06 12:35:41 -0700

    Lots of functions

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import pythonosc
-import pythonosc.dispatcher
-import pythonosc.osc_server
+from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.dispatcher import Dispatcher
 
-dispatcher = pythonosc.dispatcher
-sever = pythonosc.osc_server
+ip = "0.0.0.0"
+port = 1337
+
+
+# Shorthand for server and dispatcher
+dispatcher = Dispatcher()
+server = BlockingOSCUDPServer((ip,port), dispatcher)
+
+def test(address, *args):
+    value = args[0]
+    # value = value * 1023
+    print(f"Address: {address} \n Value: {value}")
+
+dispatcher.map("/set/parameter", test)
+dispatcher.map("/set/volume", test)
+dispatcher.map("/set/parameter/relative", test)
+dispatcher.map("/set/volume/relative", test)
+
+server.serve_forever()
+
 
