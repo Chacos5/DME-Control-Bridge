3a70058e1a48c052766e5de13f4bb53cd4e02077
Grayden <grayden.pope@gmail.com>
2025-05-07 11:33:59 -0700

    Add the bulk of the logic, prepare for serial comms

--- main.py
+++ main.py
@@ -9,16 +9,52 @@ port = 1337
 dispatcher = Dispatcher()
 server = BlockingOSCUDPServer((ip,port), dispatcher)
 
-def test(address, *args):
-    value = args[0]
-    # value = value * 1023
-    print(f"Address: {address} \n Value: {value}")
 
-dispatcher.map("/set/parameter", test)
-dispatcher.map("/set/volume", test)
-dispatcher.map("/set/parameter/relative", test)
-dispatcher.map("/set/volume/relative", test)
 
+# Serial communication function, send messages to serial device
+def sendSerial(message: str):
+    print(f"Message: {message}")
+
+
+
+
+
+# SPR (set parameter) functions
+def setParameter(index: int, value: int):
+    commandStr = f"SPR 0 {index} {value}\n"
+    sendSerial(commandStr)
+
+def setParameterHandler(address, *args):
+    if len(args) == 2:
+        setParameter(index = int(args[0]), value = int(args[1]))
+    else:
+        print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
+
+
+
+# SVOL (set volume) functions
+
+# Create command string and send to serial
+def setVolume(index: int, value: int):
+    commandStr = f"SVL 0 {index} {value}\n"
+    sendSerial(commandStr)
+
+# Callback function for osc, parse osc string, validate, then send to setVolume
+def setVolumeHandler(address, *args):
+    if len(args) == 2:
+        print(f"Command: {address} {args[0]} {args[1]}")
+        setVolume(index = int(args[0]), value = int(args[1]))
+    else:
+        print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
+
+
+
+
+# Address mapping
+dispatcher.map("/set/parameter", setParameterHandler)
+dispatcher.map("/set/volume", setVolumeHandler)
+
+
+
+print("OSC server started")
 server.serve_forever()
-
-
