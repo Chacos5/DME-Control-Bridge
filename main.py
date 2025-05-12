aa4b4de7760d3c4733f110df6b30f15bd9ff82c1
Grayden <grayden.pope@gmail.com>
2025-05-12 11:39:33 -0700

    Create default config.yaml file, add yaml parsing logic and integrate with variables

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from serial import Serial
+import yaml
 
+config = yaml.safe_load(open("config.yaml","r"))
+
+
+# Serial setup
 ser = Serial()
-ser.baudrate = 38400
-ser.port = "/dev/ttySC0"
-ser.write_timeout = 1
-print(f"\nSerial port information:\n{ser}\n")
+ser.baudrate = config["communication"]["serial"]["baudRate"]
+ser.port = config["communication"]["serial"]["port"]
+
 
 # Debug mode will print out prepared serial messages, serial enabled will disable/enable serial output
-debugMode = True
-serialEnabled = False
+debugMode = config["debugMode"]
+serialEnabled = config["serialEnabled"]
 
 
-oscIp = "0.0.0.0"
-oscPort = 1337
+# OSC network setup
+oscIp = config["communication"]["osc"]["listenIp"]
+oscPort = config["communication"]["osc"]["listenPort"]
 
 
 # Shorthand for server and dispatcher
@@ -25,15 +30,12 @@ server = BlockingOSCUDPServer((oscIp,oscPort), dispatcher)
 
 # Serial communication function, send messages to serial device
 def sendSerial(message: str):
-    
     if debugMode:
         print(f"Message: {message}")
     if serialEnabled:
         ser.open()
         ser.write(message.encode("ascii"))
         ser.close()
-
-
 
 
 
@@ -56,6 +58,26 @@ def setParameterHandler(address, *args):
         print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
 
 
+# RSPR (set parameter relative) functions
+def setParameterRel(index: int, value: int):
+    commandStr = f"RSPR 0 {index} {value}\n"
+    sendSerial(commandStr)
+
+def setParameterRelHandler(address, *args):
+    if len(args) == 2:
+        index = int(args[0])
+        value = int(args[1])
+
+        print(f"Command: {address} {index} {value}")
+        setParameterRel(index, value)
+    else:
+        print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
+
+
+
+
+
+
 
 # SVOL (set volume) functions
 
@@ -72,6 +94,22 @@ def setVolumeHandler(address, *args):
 
         print(f"Command: {address} {index} {value}")
         setVolume(index, value)
+    else:
+        print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
+
+
+# RSVL (set volume relative) functions
+def setVolumeRel(index: int, value: int):
+    commandStr = f"RSVL 0 {index} {value}\n"
+    sendSerial(commandStr)
+
+def setVolumeRelHandler(address, *args):
+    if len(args) == 2:
+        index = int(args[0])
+        value = int(args[1])
+
+        print(f"Command: {address} {index} {value}")
+        setVolumeRel(index, value)
     else:
         print(f"Incorrect OSC arguments passed! Required: 2 Passed: {len(args)}")
 
@@ -110,12 +148,17 @@ def stopWavHandler(address, *args):
 
 
 
-
 # Address mapping
+
 dispatcher.map("/set/parameter", setParameterHandler)
+dispatcher.map("/set/parameter/relative", setParameterRelHandler)
+
 dispatcher.map("/set/volume", setVolumeHandler)
+dispatcher.map("/set/volume/relative", setVolumeRelHandler)
+
 dispatcher.map("/wav/play", playWavHandler)
 dispatcher.map("/wav/stop", stopWavHandler)
+
 
 
 
